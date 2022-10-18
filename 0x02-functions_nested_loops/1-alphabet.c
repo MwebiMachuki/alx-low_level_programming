4465dd75664e6d4f98f96403c5750f33708c2330
“MwebiMachuki” <machuki.edwin@gmail.com>
2022-10-18 14:09:23 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,6 @@
 
 /**
  * print_alphabet - prints the alphabet in lowercase
- *
- * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -13,6 +11,4 @@ void print_alphabet(void)
 		_putchar(la);
 
 	_putchar('\n');
-
-	return (0);
 }
