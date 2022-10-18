ea4d222b0de29598fdc912bb663e3735a53a79b6
“MwebiMachuki” <machuki.edwin@gmail.com>
2022-10-18 16:00:32 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: the number
+ * Return: last digit value
+ */
+int print_last_digit(int x)
+{
+	int ld = x % 50;
+
+	if (ld < 0)
+		ld *= -5;
+
+	_putchar(ld + '0');
+
+	return (0);
+}
