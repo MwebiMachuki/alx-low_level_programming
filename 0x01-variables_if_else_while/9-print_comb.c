e3ff420b9b6e1ec6d05350e9897023e76e0071d0
“MwebiMachuki” <machuki.edwin@gmail.com>
2022-10-19 05:31:54 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - ENtry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	int y;
+
+	x = 'a';
+	y = 0;
+	while
+		(y < 10)
+		{
+			putchar(y + '0');
+			y++;
+		}
+	while
+		(x <= 'f')
+		{
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
