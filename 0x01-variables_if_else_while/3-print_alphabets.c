b87aa2458beac19997d3b2c8dd3c56b650efd547
“MwebiMachuki” <machuki.edwin@gmail.com>
2022-10-19 02:57:21 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	char y;
+
+	x = 'a';
+	y = 'A';
+	while
+		(x <= 'z')
+		{
+			putchar(c);
+			x++;
+		}
+	while
+		(y <= 'Z')
+		{
+			putchar(y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
