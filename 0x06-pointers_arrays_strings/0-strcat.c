7aeb7821b115f0dbcf13f6f9b273915a194c58f6
“MwebiMachuki” <machuki.edwin@gmail.com>
2022-10-28 23:27:24 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@ include "main.h"
  * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
-{
+	{
 	int x;
 	int y;
 
@@ -23,6 +23,6 @@ char *_strcat(char *dest, char *src)
 		x++;
 		y++;
 	}
-	dest[i] = '\0';
+	dest[x] = '\0';
 	return (dest);
 }
