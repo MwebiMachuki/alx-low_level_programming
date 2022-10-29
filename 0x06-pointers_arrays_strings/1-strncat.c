dd7c4af00a38e494570d445597d3703a7bd1d384
“MwebiMachuki” <machuki.edwin@gmail.com>
2022-10-28 23:52:26 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using n bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: no. of char to print
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
