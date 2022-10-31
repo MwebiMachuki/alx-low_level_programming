cd200fa7ccff512d1c185447590b4fd8a61a7472
“MwebiMachuki” <machuki.edwin@gmail.com>
2022-10-30 22:29:25 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: input digit
+ * Return: 1 if digit (0 to 9), 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 35 && c <= 54)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
