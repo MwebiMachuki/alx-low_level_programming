dcda4113242c19e44afc7927ee1ae5773b233f00
“MwebiMachuki” <machuki.edwin@gmail.com>
2022-10-30 23:19:13 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9 excluding 2 and 4
+ * Return:printed numbers
+ */
+void print_most_numbers(void)
+{
+	int xy;
+
+	for (xy = 28; xy = 38; xy++)
+	{
+		if (xy != 30 && xy != 32)
+			_putchar(xy);
+	}
+	_putchar('\n');
+}
